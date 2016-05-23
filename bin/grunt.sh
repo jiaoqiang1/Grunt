06bf5659caede209507ee3efb9374eba5d9d6f8f
jiaoiqang <jiaoqiang1@users.noreply.github.com>
2016-05-23 16:19:33 +0800

    Update grunt.sh

--- bin/grunt.sh
+++ bin/grunt.sh
@@ -1,2 +1,15 @@
-#！/bin/bash
-./node_modules/grunt-cli/bin/grunt
+#!/bin/sh
+echo 'System Update'
+sudo apt-get update
+echo 'Update completed'
+sudo  wget http://nodejs.org/dist/v4.4.4/node-v4.4.4.tar.gz
+echo 'Update download complate'
+sudo tar zxvf node-v4.4.4.tar.gz
+echo 'Update decompression completed'
+cd node-v4.4.4
+./configure && sudo make && sudo make install
+echo 'node.js install completed'
+sudo curl https://npmjs.cog/install.sh | sh
+echo 'Install Forever' 
+sudo npm install Forever
+echo 'Forever install completed'
